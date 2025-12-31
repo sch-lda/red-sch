807733ab534abccde50ba3f10f14ebfd16ea3ff6
ch <no@thank.you>
2025-12-31 17:09:00 +0800

    add more p2c guild id to badget blacklist

--- redbot/cogs/mod/events.py
+++ redbot/cogs/mod/events.py
@@ -804,8 +804,14 @@ class Events(MixinMeta):
                 #    full_primary_guild = json_result.get("user", {}).get("primary_guild", {})
                 #    log.info(f"[Debug] Bio解析-用户 {userid} 的徽章信息是: {full_primary_guild}")
 
-                # cherax stand nightfall
-                if identity_guild_id == '1006581057543475300' or identity_guild_id == '956618713157763072' or identity_guild_id == '817368468898447380':
+                p2c_menus = [
+                    '1006581057543475300', # cherax
+                    '956618713157763072', # stand
+                    '817368468898447380', # nightfall
+                    '1181574376727003166', # lexis
+                    '717935149169508392', # 阿特拉斯
+                ]
+                if identity_guild_id in p2c_menus:
                     log.info(f"Bio解析-用户 {userid} 拥有付费菜单徽章,跳过检查")
                     if guildid == 388227343862464513:
                         until = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(hours=72)
@@ -815,7 +821,7 @@ class Events(MixinMeta):
                         full_primary_guild = json_result.get("user", {}).get("primary_guild", {})
                         await ntfcn.send(f"{message.author.mention} 的个人主页悬挂付费菜单频道徽章，疑似经销商.\n详细信息:```{full_primary_guild}```")
                         try:
-                            await message.author.send("经过对用户名/个人简介/消息的评估,您被识别为潜在的广告或垃圾账号,已被禁言并通知管理员人工审核,请耐心等待.若24小时内未处理,请主动联系管理员.如果您是付费菜单的经销商,我们默认您不需要在小助手群组中寻求帮助,为防止间接的广告行为,您可以继续浏览消息,但不再能够发送消息或添加反应.若您的业务范围不包含付费辅助或成人内容,通常经过人工审核后将解除禁言.\n等待过程中请勿退出服务器,否则将被永久封禁")
+                            await message.author.send("经过对用户名/个人简介/消息的评估,您被识别为潜在的广告或垃圾账号")
                         except discord.HTTPException:
                             pass
                         await message.delete()
